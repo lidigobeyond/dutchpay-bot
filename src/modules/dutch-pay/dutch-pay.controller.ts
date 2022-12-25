2da548f835de352c996f8e063b141687244da8ba
Kihyun Kim <khkh4631@gmail.com>
2022-12-25 11:16:07 +0900

    refactor(dutch-pay): 사용자 호출에 대한 응답을 반환하는 역할을 컨트롤러에 위임함

--- src/modules/dutch-pay/dutch-pay.controller.ts
+++ src/modules/dutch-pay/dutch-pay.controller.ts
@@ -1,16 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SlashCommandPayload } from '../slack/dto/payloads/slash-command.payload';
 import { ClassTransformPipe } from '../../common/pipes/class-transform.pipe';
-import { DutchPayService } from './dutch-pay.service';
+import { SlackService } from '../slack/slack.service';
+import { DutchPayModal } from '../slack/dto/modals/dutch-pay.modal.dto';
 
 @Controller('dutch-pay')
 export class DutchPayController {
-  constructor(private readonly dutchPayService: DutchPayService) {}
+  constructor(private readonly slackService: SlackService) {}
 
   @Post('slash-command-was-invoked')
-  slashCommandWasInvoked(@Body(ClassTransformPipe) slashCommandPayload: SlashCommandPayload): Promise<void> {
+  async slashCommandWasInvoked(@Body(ClassTransformPipe) slashCommandPayload: SlashCommandPayload): Promise<void> {
     const { triggerId, text: title } = slashCommandPayload;
 
-    return this.dutchPayService.openNewDutchPayModal(triggerId, title);
+    await this.slackService.openModal(triggerId, new DutchPayModal(title));
   }
 }
--- src/modules/dutch-pay/dutch-pay.service.ts
+++ src/modules/dutch-pay/dutch-pay.service.ts
@@ -1,12 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { SlackService } from '../slack/slack.service';
-import { DutchPayModal } from '../slack/dto/modals/dutch-pay.modal.dto';
 
 @Injectable()
-export class DutchPayService {
-  constructor(private readonly slackService: SlackService) {}
-
-  async openNewDutchPayModal(triggerId: string, title?: string): Promise<void> {
-    await this.slackService.openModal(triggerId, new DutchPayModal(title));
-  }
-}
+export class DutchPayService {}
